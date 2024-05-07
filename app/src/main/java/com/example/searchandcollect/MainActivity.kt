1f1f2c09fa1050376b48bbe9202075d034651abf
theBettor <be0128st@gmail.com>
2024-05-07 16:24:40 +0900

    MyStorageFragment
    데이터, 멀티뷰타입, api 남았다 일단은

--- app/src/main/java/com/example/searchandcollect/MainActivity.kt
+++ app/src/main/java/com/example/searchandcollect/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.searchandcollect.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -23,17 +24,42 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.add(binding.frame.id, SearchFragment())
-        fragmentTransaction.commit()
+
         // 뷰페이저는 사용하기 싫었다. 탭메인을 사용하기 위해 변수로 만들어주고,
         // 사용할 이미지들을 리스트 변수로 만들고 for문을 통해 index로 구현했다.
         val tabLayout = binding.tapMain
         val tabIcons = listOf(R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground)
 
+
+        val fragmentList = listOf(
+            SearchFragment(), // 인덱스 0에 해당하는 프래그먼트
+            MyStorageFragment() // 인덱스 1에 해당하는 다른 프래그먼트
+            // 필요한 만큼 추가
+        )
+        setFragment(SearchFragment())
+
         for (i in tabIcons.indices) {
             val tab = tabLayout.getTabAt(i)
             tab?.setIcon(tabIcons[i])
         }
+
+        // 각 탭에 대한 클릭 리스너 설정
+        for (i in fragmentList.indices) {
+            val tab = tabLayout.getTabAt(i)
+            if (tab != null) {
+                tab.view.setOnClickListener { // view 넣어주니까 해결됨
+                    // 클릭한 탭에 해당하는 프래그먼트로 교체
+                    setFragment(fragmentList[i])
+                }
+            }
+        }
+    }
+
+    // 프래그먼트 교체 함수 정의
+    private fun setFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.framelayout, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
